943a7e9f064fcbab41dcacd0dc28fec9566d6714
darrylweimers <darrylweimers@gmail.com>
2018-10-22 22:40:50 -0400

    update conditions and loops

--- src/loop_example.py
+++ src/loop_example.py
@@ -1,74 +1,92 @@
 
+from threading import Timer as Timeout
+
 """
-For loop
-
-     Repeat body if condition is true
-
-for item in items:           # Assign an item from items
+    While loop
+    
+    Repeat body if condition is true
+    
+    General format:
+    
+    while condition:            # if condition is true, execute body
     statement(s)
-else:                        # execute if loop never executes
+    else:                       # execute if loop body never executes
     statement(s)
-"""
+    """
 
-for item in range(0):          # Assign a value from range of 0 to 5 to item
-    # body: statement(s)
-    print(item, end=", ")
+SIZE_OF_HARD_DRIVE_IN_GB = 4
+storage_used_in_gb = 4
+
+while storage_used_in_gb < SIZE_OF_HARD_DRIVE_IN_GB:
+    remaining_storage = SIZE_OF_HARD_DRIVE_IN_GB - storage_used_in_gb
+    print("Remaining storage space: {remaining}".format(remaining=remaining_storage))
+    storage_used_in_gb += 1
 else:
-    print("execute if loop never executes")
+    print("Hard drive is full.")
+
+def temperature_drop():
+    global tea_temperature
+    tea_temperature = tea_temperature - 10
+    print("Tea's temperature: {temperature}".format(temperature=tea_temperature))
+
+number_of_seconds = 5
+tea_temperature_drop_over_time = Timeout(number_of_seconds, temperature_drop)
 
 
 
 
+"""
+    While loop
+    
+    Repeat body if condition is true
+    
+    General format:
+    
+    while condition:            # if condition is true, execute body
+    statement(s)
+    else:                       # execute if loop body never executes
+    statement(s)
+    """
+
+
+tea_temperature = 50
+print("Tea temperature: {temperature}".format(temperature=tea_temperature))
+
+tea_temperature_drop_over_time.start()
+
+while tea_temperature > 40:
+    pass
+
+print("Tea is warm enough to drink.")
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-# """
-# While loop
+# For loop
 #
-#     Repeat body if condition is true
+#      Repeat body if condition is true
 #
-# General format:
-#
-# while condition:            # if condition is true, execute body
+# for item in items:           # Assign an item from items
 #     statement(s)
-# else:                     # execute if loop body never executes
+# else:                        # execute if loop never executes
 #     statement(s)
-# """
-#
-# count = 0
-#
-# while count:        # condition: count is greater than 0
-#     # body: statement(s)
-#     print(count, end=", ")
-#     count -= 1
-# else:
-#     print("execute if loop body never executes")
 
 
 
+transactions = [25, 45, 50, 34]
 
+total = 0
 
+item = 1
 
+for transaction in transactions:
+    if transaction == 45:
+        continue
+    print("Item {item}: {price}".format(item=item, price=transaction))
+    total += transaction
+    item += 1
+    if total > 60:
+        print("You have exceed your weekly budget.")
+        break
 
-
-
-
-
-
-
-
-
-
-
-
+print("Total: {total}".format(total=total))
